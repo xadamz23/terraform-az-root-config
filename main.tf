604c3bb7e083279c9954e6b7c072e18e3eb6959d
Adam Stahl <adam.stahl@cbre.com>
2026-05-20 15:36:59 -0500

    used claude to introduce break/fix and additional concepts

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "storage" {
   #   source = "../terraform-az-storage-module"
-  source = "git::ssh://git@github.com/xadamz23/terraform-az-storage-module//storage?ref=1.0.1"
+  source = "git::ssh://git@github.com/xadamz23/terraform-az-storage-module//storage?ref=1.0.2"
 
   resource_group_name      = var.resource_group_name
   location                 = var.location
   storage_account_name     = var.storage_account_name
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-  tags                     = var.tags
+  tags                     = local.common_tags
 }
--- outputs.tf
+++ outputs.tf
@@ -7,5 +7,10 @@ output "storage_account_name" {
 }
 
 output "storage_account_id" {
-  value = module.storage.storage_account_id
+  value     = module.storage.storage_account_id
+  sensitive = true
+}
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
 }
