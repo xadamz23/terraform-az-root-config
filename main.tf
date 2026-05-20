9d0e7db33bbaaac6ba2738aef7789ea52ece2fe0
Adam Stahl <adam.stahl@cbre.com>
2026-05-20 10:35:46 -0500

    updated module source from a local reference to referencing the modules git repo

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "storage" {
-  source = "../terraform-az-storage-module"
+  #   source = "../terraform-az-storage-module"
+  source = "git::ssh://git@github.com/xadamz23/terraform-az-storage-module//storage?ref=1.0.1"
 
   resource_group_name      = var.resource_group_name
   location                 = var.location
